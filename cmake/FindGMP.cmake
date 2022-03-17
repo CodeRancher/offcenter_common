b7f7fab7ccf8d616b6cdd77f98433cb4b14d71f1
CodeRancher <37150971+CodeRancher@users.noreply.github.com>
2022-03-16 22:49:39 -0600

    Modified cmake find for GMP and MPFR to search for static libs

---
+++ cmake/FindGMP.cmake
@@ -0,0 +1,24 @@
+# Try to find the GNU Multiple Precision Arithmetic Library (GMP)
+# See http://gmplib.org/
+
+if (GMP_INCLUDES AND GMP_LIBRARIES)
+  set(GMP_FIND_QUIETLY TRUE)
+endif (GMP_INCLUDES AND GMP_LIBRARIES)
+
+find_path(GMP_INCLUDES
+  NAMES
+  gmp.h
+  PATHS
+  $ENV{GMPDIR}
+  ${INCLUDE_INSTALL_DIR}
+)
+
+find_library(GMP_LIBRARIES NAMES libgmp.a gmp PATHS $ENV{GMPDIR} ${LIB_INSTALL_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GMP DEFAULT_MSG
+                                  GMP_INCLUDES GMP_LIBRARIES)
+mark_as_advanced(GMP_INCLUDES GMP_LIBRARIES)
+
+message(STATUS "gmp Includes:                   ${GMP_INCLUDES}")
+message(STATUS "gmp Libraries:                  ${GMP_LIBRARIES}")
